8ecb3b3b00c009a024057872a264f93412b52677
Piotr <piotrmalyszek1@gmail.com>
2022-10-06 22:16:52 +0200

    Registration, login and authorization

---
+++ LetsMeet.API/LetsMeet.API/Migrations/20221006175454_user_v1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LetsMeet.API.Migrations
+{
+    public partial class user_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FieldOfStudies",
+                table: "Users",
+                newName: "Major");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Users",
+                newName: "Bio");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Major",
+                table: "Users",
+                newName: "FieldOfStudies");
+
+            migrationBuilder.RenameColumn(
+                name: "Bio",
+                table: "Users",
+                newName: "Description");
+        }
+    }
+}
